b984317bbd94b25602cf073eb33050deb105ff43
SVilcaLim03 <66433673+SVilcaLim03@users.noreply.github.com>
2022-07-17 17:21:42 -0500

    It's For Dennis

--- views/editar_pregunta.php
+++ views/editar_pregunta.php
@@ -9,16 +9,12 @@
     <h1> Editar pregunta </h1>
     <br><br>
 
-        <form name="form_editar_pregunta" action="../controllers/editar_pregunta.php" method="post">
-
+        <form name="form_editar_pregunta" action="../controllers/pregunta.php" method="post">
+        <input type="hidden" name="type" value="edit">
         Id: <input name="id" type="text"/>  <br/><br/>
         Titulo: <input name="titulo" type="text"/>  <br/><br/>
         Descripcion:  <textarea name="descripcion"> </textarea> <br/><br/>
         <input type="submit" value="Enviar ahora"/>
-      
         </form>
-    
-    
 </body>
-
 </html>
